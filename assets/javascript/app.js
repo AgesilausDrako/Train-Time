c4cde3be1875f91336ac09ee647a191506de6b1c
AgesilausDrako <agesilaus@yahoo.com>
2017-07-12 13:57:38 -0400

    added firebase info

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+var config = {
+    apiKey: "AIzaSyAr0Ale4f3tuWsdhqhkujyRwxFYWQtSXI4",
+    authDomain: "claybase-83c0d.firebaseapp.com",
+    databaseURL: "https://claybase-83c0d.firebaseio.com",
+    projectId: "claybase-83c0d",
+    storageBucket: "claybase-83c0d.appspot.com",
+    messagingSenderId: "198022101030"
+};
+
+firebase.initializeApp(config);
+
+
+// Create a variable to reference the database.
+var database = firebase.database();
